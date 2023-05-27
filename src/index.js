ccde1040078a19e13a00e151f4260762ad825684
Andrey Emelianov <621077@mail.ru>
2023-05-27 19:02:02 +0300

    сделал обработку данных приходящих от сервера и реализовал подргрузку рандомного персонажа в компоненте randomChar

--- src/index.js
+++ src/index.js
@@ -2,15 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/App';
 
-import MarvelService from './services/MarvelService';
-
 import './style/style.scss';
-
-const marvelService = new MarvelService();
-
-marvelService.getAllCharacters().then((res) => {
-	return res.data.results.forEach((item) => console.log(item.name));
-});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
