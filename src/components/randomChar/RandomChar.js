5bd8d991f0bc0220ea0fc33ce6bdcebdaf4ee10b
Andrey Emelianov <621077@mail.ru>
2023-05-28 15:20:13 +0300

    Реализовал компонент CharList отображающий 9 персонажей на странице

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -29,8 +29,15 @@ class RandomChar extends Component {
 		this.setState({ char, loading: false });
 	};
 
+	onCharLoading = () => {
+		this.setState({
+			loading: true,
+		});
+	};
+
 	updateChar = () => {
 		const id = Math.floor(Math.random() * (1011400 - 1011000) + 1011000);
+		this.onCharLoading();
 		this.marvelService.getCharacter(id).then(this.onCharLoaded).catch(this.onError);
 	};
 
@@ -61,7 +68,9 @@ class RandomChar extends Component {
 					</p>
 					<p className="randomchar__title">Or choose another one</p>
 					<button className="button button__main">
-						<div className="inner">try it</div>
+						<div className="inner" onClick={this.updateChar}>
+							try it
+						</div>
 					</button>
 					<img src={mjolnir} alt="mjolnir" className="randomchar__decoration" />
 				</div>
@@ -73,9 +82,22 @@ class RandomChar extends Component {
 const View = ({ char }) => {
 	const { name, description, thumbnail, homepage, wiki } = char;
 
+	let imgStyle = { objectFit: 'cover' };
+
+	if (
+		thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg'
+	) {
+		imgStyle = { objectFit: 'contain' };
+	}
+
 	return (
 		<div className="randomchar__block">
-			<img src={thumbnail} alt="Random character" className="randomchar__img" />
+			<img
+				src={thumbnail}
+				alt="Random character"
+				className="randomchar__img"
+				style={imgStyle}
+			/>
 			<div className="randomchar__info">
 				<p className="randomchar__name">{name}</p>
 				<p className="randomchar__descr">{description}</p>
