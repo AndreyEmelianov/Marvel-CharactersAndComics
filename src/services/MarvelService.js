21d947dea3f449987213a956583f2e80a468b007
Andrey Emelianov <621077@mail.ru>
2023-05-30 16:13:47 +0300

    обновил react router до последней версии, сделал страничку 404, реализовал компонент по отображению одного выбранного комикса

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -29,9 +29,8 @@ const useMarvelService = () => {
 		return res.data.results.map(_transformComics);
 	};
 
-	const getComics = async (id) => {
+	const getComic = async (id) => {
 		const res = await request(`${_apiBase}comics/${id}?${_apiKey}`);
-
 		return _transformComics(res.data.results[0]);
 	};
 
@@ -52,10 +51,10 @@ const useMarvelService = () => {
 	const _transformComics = (comics) => {
 		return {
 			id: comics.id,
-			title: comics.title,
+			// title: comics.title,
 			description: comics.description || 'There is no description',
 			pageCount: comics.pageCount
-				? `${comics.pageCount} p`
+				? `${comics.pageCount} p.`
 				: 'No information about the number of pages',
 			thumbnail: comics.thumbnail.path + '.' + comics.thumbnail.extension,
 			language: comics.textObjects[0]?.language || 'en-us',
@@ -70,7 +69,7 @@ const useMarvelService = () => {
 		getCharacter,
 		clearError,
 		getAllComics,
-		getComics,
+		getComic,
 	};
 };
 
